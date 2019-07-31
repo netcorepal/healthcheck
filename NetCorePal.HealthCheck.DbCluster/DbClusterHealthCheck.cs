c876b602f738d42b14b7917b2642140f0526dc8b
xuchao <chao.xu@xiaobao100.com>
2019-07-31 19:15:34 +0800

    增加DbCluster检查支持

---
+++ NetCorePal.HealthCheck.DbCluster/DbClusterHealthCheck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCorePal.HealthCheck.DbCluster
+{
+    /// <summary>
+    /// 面向.NetFramework数据库配置（集群版）的健康检查
+    /// </summary>
+    public class DbClusterHealthCheck : HealthChecker
+    {
+        private readonly DbProviderFactory connectionFactory;
+        
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="name">Checker 自定义名称</param>
+        /// <param name="connectionFactory">rebbitmq 连接工厂对象</param>
+        public DbClusterHealthCheck(string name, string connectionString, string provider = "MySql.Data.MySqlClient")
+        {
+            this.Name = name;
+            this.connectionFactory = DbProviderFactories.GetFactory(provider);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override HealthCheckResult Check()
+        {
+            return CheckAsync().Result;
+        }
+
+        public override async Task<HealthCheckResult> CheckAsync()
+        {
+            using (var connection = this.connectionFactory.CreateConnection())
+            {
+                await connection.OpenAsync();
+                return new HealthCheckResult { Name = this.Name, IsHealthy = true };
+            }
+        }
+    }
+}
---
+++ NetCorePal.HealthCheck.DbCluster/HealthCheckerManagerExtenstions.cs
@@ -0,0 +1,39 @@
+﻿using NetCorePal.HealthCheck.DbCluster;
+using System.Configuration;
+
+namespace NetCorePal.HealthCheck
+{
+    /// <summary>
+    /// Extenstions for <see cref="HealthCheckerManager"/> 
+    /// </summary>
+    public static class HealthCheckerManagerExtenstions
+    {
+        /// <summary>
+        /// add  <see cref="DbClusterHealthCheck"/> to manager
+        /// </summary>
+        /// <param name="manager"></param>
+        /// <returns></returns>
+        public static HealthCheckerManager AddDbClusterHealthChecker(this HealthCheckerManager manager, string dbclusterSection = "databaseClusters")
+        {
+            var cluster = ConfigurationManager.GetSection(dbclusterSection) as SchoolPal.Toolkit.Configuration.DatabaseClusters.DatabaseClusterSection;
+
+            if (cluster != null)
+            {
+                var clusters = cluster.GetClusters();
+                foreach (var conn in clusters)
+                {
+                    manager.Add(new DbClusterHealthCheck(conn.Name, conn.Master));
+
+                    var slaves = conn.GetSlaveConnectionStrings();
+                    for (int i = 0; i < slaves.Length; i++)
+                    {
+                        manager.Add(new DbClusterHealthCheck($"{conn.Name}:Slave{i}", slaves[i]));
+                    }
+
+                }
+            }
+
+            return manager;
+        }
+    }
+}
