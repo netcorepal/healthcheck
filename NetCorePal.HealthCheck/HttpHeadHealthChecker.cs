9284f2aa3f4a908c619cd1e58035d938a6d79974
xuchao <chao.xu@xiaobao100.com>
2019-08-02 15:56:24 +0800

    增加一个专门针对Head Method方式的健康检查

---
+++ NetCorePal.HealthCheck/HttpHeadHealthChecker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace NetCorePal.HealthCheck
+{
+    /// <summary>
+    /// HttpHealthChecker
+    /// </summary>
+    public class HttpHeadHealthChecker : IHealthChecker
+    {
+        readonly string url;
+
+        readonly HttpClient client;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name">checker name</param>
+        /// <param name="url">http url to check</param>
+        public HttpHeadHealthChecker(string name, string url)
+        {
+            this.Name = name;
+            this.url = url;
+            this.client = new HttpClient
+            {
+                Timeout = TimeSpan.FromSeconds(15)
+            };
+            this.client.DefaultRequestHeaders.Add("health-checker", "httpclient");
+        }
+        /// <summary>
+        /// checker name
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<HealthCheckResult> CheckAsync()
+        {
+            await client.SendAsync(new HttpRequestMessage
+            {
+                Method = new HttpMethod("HEAD"),
+                RequestUri = new Uri(url)
+            }).ConfigureAwait(false);
+
+            return null;
+        }
+    }
+}
